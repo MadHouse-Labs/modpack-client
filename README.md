0c3ebe75ba6fab4249f2d07e3d38eee1d0025f0b
cirroskais <cirroskais@madhouselabs.net>
2024-02-23 06:49:14 -0500

    Add readme whatever im going to bed

---
+++ README.md
@@ -0,0 +1,18 @@
+# modpack-client
+
+Automatically updates your modpack mods. That's it.
+
+## Compiling
+
+```bash
+yarn add
+yarn run build:win
+# bingo bango bongo your binary is in build/
+```
+
+## Todo
+
+-   GitHub Actions automatic builds
+-   Fetch mods from CurseForge
+-   Commit changes to the filesystem
+-   ~~Distribute the malware~~
--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,7 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 
-const STORAGE_FILE = "data.json";
+/*
+    process.cwd() while running under PrismLauncher
+    is always going to be $INST_MC_DIR
+
+    This means that STORAGE_FILE is going to be in
+    .minecraft, and not the instance folder
+
+    This also means that to access the mods folder,
+    we will have to path through process.cwd()/mods
+*/
+
+const STORAGE_FILE = "modpack-data.json";
 
 try {
     fs.readFileSync(path.join(process.cwd(), STORAGE_FILE), {
