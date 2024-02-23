9a207639e82e87b6a8060d1d6885c57b4eee857e
cirroskais <cirroskais@madhouselabs.net>
2024-02-23 01:07:13 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { info, warn, error, success } from "./lib/logger.js";
+
+info("info log");
+warn("warn log");
+error("error log");
+success("success log");
---
+++ src/lib/logger.js
@@ -0,0 +1,59 @@
+// COLOR: [ FG, BG ]
+const colors = {
+    fg: (_) => {
+        return "\x1b[" + _[0] + "m";
+    },
+    bg: (_) => {
+        return "\x1b[" + _[1] + "m";
+    },
+    white: [37, 47],
+    blue: [34, 44],
+    red: [31, 41],
+    black: [30, 40],
+    yellow: [33, 43],
+    green: [32, 42],
+};
+
+export function info(...args) {
+    console.log(
+        colors.bg(colors.blue) +
+            colors.fg(colors.black) +
+            "[ INF ]" +
+            colors.bg(colors.black) +
+            colors.fg(colors.white),
+        ...args
+    );
+}
+
+export function warn(...args) {
+    console.log(
+        colors.bg(colors.yellow) +
+            colors.fg(colors.black) +
+            "[ WRN ]" +
+            colors.bg(colors.black) +
+            colors.fg(colors.white),
+        ...args
+    );
+}
+
+export function error(...args) {
+    console.log(
+        colors.bg(colors.red) +
+            colors.fg(colors.black) +
+            "[ ERR ]" +
+            colors.bg(colors.black) +
+            colors.fg(colors.white),
+        ...args
+    );
+}
+
+export function success(...args) {
+    console.log(
+        colors.bg(colors.green) +
+            colors.fg(colors.black) +
+            "[ SUC ]" +
+            colors.bg(colors.black) +
+            colors.fg(colors.white),
+        ...args
+    );
+}
